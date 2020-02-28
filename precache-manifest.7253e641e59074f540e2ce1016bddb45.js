abaae7b668f986559d86050c16eb722dc0cfe27c
Hector Murillo Angulo <c21.hector@gmail.com>
2020-02-27 23:30:36 -0700

    as

---
+++ precache-manifest.7253e641e59074f540e2ce1016bddb45.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9967d30f7c8d61f5629f",
+    "url": "./css/app.c8f6eb13.css"
+  },
+  {
+    "revision": "d321f90a4091e4af37f2",
+    "url": "./css/chunk-vendors.efae5c10.css"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "./fonts/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "./fonts/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "./fonts/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "./fonts/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "./fonts/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "./fonts/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "./fonts/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "./fonts/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "./fonts/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "./fonts/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "./fonts/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "./fonts/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "53f6b5f8d9a3d7586c9f6df678b2eaa6",
+    "url": "./img/concretos.53f6b5f8.jpg"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "./img/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "./img/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "./img/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "47223afeeda573413e62eb24f55348ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9967d30f7c8d61f5629f",
+    "url": "./js/app.e012e186.js"
+  },
+  {
+    "revision": "d321f90a4091e4af37f2",
+    "url": "./js/chunk-vendors.c99b4a2e.js"
+  },
+  {
+    "revision": "5f2f66a972ef7d3a6adb29008b8731f4",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "./robots.txt"
+  }
+]);
